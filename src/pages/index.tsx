2081a4d8865794e69429c06ed05591407e049e66
abhinav <contact.abhinavanshul@gmail.com>
2023-09-26 15:12:45 +0530

    feat: initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head"
+import { Inter } from "next/font/google"
+import { useQuery } from "@tanstack/react-query"
+import { Fragment } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  weight: "400",
+  subsets: ["latin"],
+})
 
 export default function Home() {
+  const fetchPokemon = async (pokemonNumber: any) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`).then((res) =>
+      res.json()
+    )
+    return res // Return the Pokémon data
+  }
+
+  const fetchPokemonArray = async () => {
+    const pokemonArray = []
+    for (let i = 1; i <= 30; i++) {
+      try {
+        const pokemonData = await fetchPokemon(i)
+        pokemonArray.push(pokemonData)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    return pokemonArray
+  }
+
+  const {
+    isLoading,
+    error,
+    data: pokemon,
+  } = useQuery([`fetch-top-20-pokemon`], () => fetchPokemonArray())
+
+  console.log({ pokemon })
+
   return (
     <>
       <Head>
@@ -14,108 +45,27 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+      <main className={inter.className}>
+        <h1 style={{ textAlign: "center", margin: "4rem" }}>Pokedex</h1>
+        {isLoading && <h2>Loading...</h2>}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            alignItems: "center",
+            justifyContent: "center",
+            justifySelf: "center",
+          }}
+        >
+          {pokemon?.map((itm, index) => (
+            <div
+              style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+              key={index}
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+              <img alt={itm?.name} src={itm?.sprites?.front_default} />
+              <div>{itm?.name}</div>
+            </div>
+          ))}
         </div>
       </main>
     </>
